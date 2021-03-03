d7282d51286aec7929124a285a9e58a5eae5776a
martinezgomez <emartinezgomez1985@gmail.com>
2021-03-03 22:59:27 +0100

    push para descargar alli

---
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-dinamicos',
+  templateUrl: './dinamicos.component.html',
+  styles: [
+  ]
+})
+export class DinamicosComponent {
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [ '', [Validators.required, Validators.minLength(3)]]
+  })
+
+  constructor(private fb: FormBuilder) { }
+  
+  campoEsValido( campo: string ){
+    return this.miFormulario.controls[campo].errors 
+            && this.miFormulario.controls[campo].touched
+  }
+
+  guardar(){
+    if(this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+ 
+    console.log(this.miFormulario.value)
+  }
+
+}
