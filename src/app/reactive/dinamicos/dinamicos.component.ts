231b65be8f1b3e6b3b210e36c09319fbda8df292
martinezgomez <emartinezgomez1985@gmail.com>
2021-03-09 09:51:59 +0100

    reactivos acabados

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -10,23 +10,50 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class DinamicosComponent {
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: [ '', [Validators.required, Validators.minLength(3)]]
-  })
+    nombre: ['', [ Validators.required, Validators.minLength(3) ] ],
+    favoritos: this.fb.array([
+      ['Metal Gear'],
+      ['Gears of War']
+    ], Validators.required)
+  });
+
+  nuevoFavorito: FormControl = this.fb.control('', Validators.required);
+
+  get favoritosArr(){
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
 
   constructor(private fb: FormBuilder) { }
-  
-  campoEsValido( campo: string ){
-    return this.miFormulario.controls[campo].errors 
-            && this.miFormulario.controls[campo].touched
+
+  nombreValido( nombre: string) {
+    console.log('estoy en validar')
+    console.log(this.miFormulario.controls[nombre].errors 
+      && this.miFormulario.controls[nombre].touched)
+
+    return this.miFormulario.controls[nombre].errors 
+            && this.miFormulario.controls[nombre].touched;
+  }
+
+  agregarFavorito(){
+    if( this.nuevoFavorito.invalid ){return;}
+    
+    this.favoritosArr.push( this.fb.control( this.nuevoFavorito.value, Validators.required ) );
+
+    this.nuevoFavorito.reset()
+  }
+
+  borrar(i: number){
+    this.favoritosArr.removeAt(i)
   }
 
   guardar(){
     if(this.miFormulario.invalid){
-      this.miFormulario.markAllAsTouched();
+      this.miFormulario.markAllAsTouched
       return;
     }
- 
+    console.log('valido')
     console.log(this.miFormulario.value)
-  }
+
+  } 
 
 }
